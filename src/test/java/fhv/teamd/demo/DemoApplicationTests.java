ca6b31c79ee096822ce9df2b2e631425a4001e86
Jeremias HEEL <150070@htlr.onmicrosoft.com>
2021-10-22 11:27:24 +0200

    setup from start.spring.io with maven

---
+++ src/test/java/fhv/teamd/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fhv.teamd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
