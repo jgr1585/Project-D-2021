e78444411dac860c5347486b5f41a4fd5c43418f
jhe6245 <jhe6245@students.fhv.at>
2021-10-26 20:59:31 +0200

    add placeholders for index & booking pages

--- src/main/java/fhv/teamd/demo/DemoApplication.java
+++ src/main/java/fhv/teamd/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package fhv.teamd.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
@@ -13,9 +15,19 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "hello world";
+	@GetMapping("/")
+	public ModelAndView index() {
+		return new ModelAndView("index");
+	}
+
+	@GetMapping("/booking/chooseCategories")
+	public ModelAndView createBooking() {
+		return new ModelAndView("booking/chooseCategories");
+	}
+
+	@GetMapping("/booking/personalDetails")
+	public ModelAndView personalDetails() {
+		return new ModelAndView("booking/personalDetails");
 	}
 
 }
