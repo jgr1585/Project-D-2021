bd748ddd6d2514dafe8b0e98316e7d37f33568bd
Jeremias HEEL <150070@htlr.onmicrosoft.com>
2021-10-27 10:24:21 +0200

    shitty little forms testing redirect for validation purposes

--- src/main/java/fhv/teamd/demo/DemoApplication.java
+++ src/main/java/fhv/teamd/demo/DemoApplication.java
@@ -4,13 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.http.HttpResponse;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -30,4 +35,17 @@ public class DemoApplication {
 		return new ModelAndView("booking/personalDetails");
 	}
 
+	@RequestMapping("/booking/submitCategories")
+	public String submitCategories(HttpServletResponse response) throws IOException {
+		// to do: read parameters and check for availability here
+
+		// not available or other error: redirect back, with error msg
+		//response.sendRedirect("/booking/chooseCategories");
+
+		// success: redirect to next step
+		response.sendRedirect("/booking/personalDetails");
+
+		// this is irrelevant
+		return "hello";
+	}
 }
