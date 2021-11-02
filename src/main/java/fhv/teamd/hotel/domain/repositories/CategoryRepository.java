092eb7f1621b848468a71d237ed736e28e833ed5
jhe6245 <jhe6245@students.fhv.at>
2021-11-02 19:56:12 +0100

    CategoryRepository

---
+++ src/main/java/fhv/teamd/hotel/domain/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package fhv.teamd.hotel.domain.repositories;
+
+import fhv.teamd.hotel.domain.Category;
+
+import java.util.List;
+
+public interface CategoryRepository {
+
+    List<Category> getAll();
+
+
+}
