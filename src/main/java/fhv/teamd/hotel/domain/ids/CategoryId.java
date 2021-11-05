4e6a60ed8123953a77eb2cc87364f15a0aad3d0c
Jeremias HEEL <150070@htlr.onmicrosoft.com>
2021-11-05 16:32:02 +0100

    ids toString() - important for converting to dto

--- src/main/java/fhv/teamd/hotel/domain/ids/CategoryId.java
+++ src/main/java/fhv/teamd/hotel/domain/ids/CategoryId.java
@@ -28,4 +28,9 @@ public class CategoryId {
     public int hashCode() {
         return Objects.hash(this.id);
     }
+
+    @Override
+    public String toString() {
+        return this.id;
+    }
 }
