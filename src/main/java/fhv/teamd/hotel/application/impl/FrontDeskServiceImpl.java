f3968736a1cf07449b3b1093e9b99ca56a1717b7
jhe6245 <jhe6245@students.fhv.at>
2021-11-13 19:32:26 +0100

    implement check in application service

---
+++ src/main/java/fhv/teamd/hotel/application/impl/FrontDeskServiceImpl.java
@@ -0,0 +1,56 @@
+package fhv.teamd.hotel.application.impl;
+
+import fhv.teamd.hotel.application.FrontDeskService;
+import fhv.teamd.hotel.domain.Room;
+import fhv.teamd.hotel.domain.Stay;
+import fhv.teamd.hotel.domain.contactInfo.GuestDetails;
+import fhv.teamd.hotel.domain.contactInfo.RepresentativeDetails;
+import fhv.teamd.hotel.domain.ids.RoomId;
+import fhv.teamd.hotel.domain.repositories.RoomRepository;
+import fhv.teamd.hotel.domain.repositories.StayRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Component
+public class FrontDeskServiceImpl implements FrontDeskService {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private StayRepository stayRepository;
+
+    @Transactional
+    @Override
+    public void checkIn(List<String> roomIds, Duration expectedDuration,
+                        GuestDetails guest, RepresentativeDetails representative) {
+
+        Set<Room> rooms = new HashSet<>();
+
+        for(String roomId: roomIds) {
+
+            Optional<Room> result = this.roomRepository.getById(new RoomId(roomId));
+
+            if(result.isEmpty()) {
+                throw new EntityNotFoundException("invalid room id");
+            }
+
+            rooms.add(result.get());
+        }
+
+
+        LocalDateTime checkIn = LocalDateTime.now();
+        LocalDateTime checkOut = checkIn.plus(expectedDuration);
+
+
+        Stay newStay = new Stay(this.stayRepository.nextIdentity(), checkIn, checkOut, rooms, guest, representative);
+
+        this.stayRepository.put(newStay);
+    }
+}
