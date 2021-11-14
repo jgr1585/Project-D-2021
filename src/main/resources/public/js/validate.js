1449843caf804ae9bb77bdffa3018eb981758b7f
jgr1585 <jgr1585@students.fhv.at>
2021-11-14 19:12:27 +0100

    Added Validation for chooseCategories.html

--- src/main/resources/public/js/validate.js
+++ src/main/resources/public/js/validate.js
@@ -1,16 +1,69 @@
 //add html validation to Page
 
+//Add new Check for Date
+$.validator.addMethod("minDateToday", function(value, element) {
+    let curDate = new Date();
+    let inputDate = new Date(value);
+    if (inputDate >= curDate) {
+        return true;
+    } else {
+        return false;
+    }
+}, "Invalid Date!");
+
+//Require at lest n in class
+$.validator.addMethod("atLeastNInClass", function (value, element, param) {
+    let count = 0;
+
+    $(param.class).each( function() {
+            count += $(this).val();
+    });
+
+    if (count >= param.min) {
+        return true;
+    } else {
+        return false;
+    }
+
+}, "Values are below Minimum!");
+
+//Compare Date
+$.validator.addMethod("validateDate", function (value, element) {
+    let from = new Date($("#from").val());
+    let until = new Date($("#until").val());
+
+
+    if (from <= until) {
+        return true;
+    } else {
+        return false;
+    }
+
+}, "From is after Until!");
 
 
 $(document).ready(function () {
+
+    //Init Form
+    $('form').each(function() {
+        $(this).validate();
+    });
+
     //Date not Null
-    $("#from #until").validate({
-       rules: {
-           field: {
-               required: true
-           }
-       }
+    $("input[type='date']").rules("add", {
+        minDateToday: true,
+        required: true,
+        validateDate: true
     });
 
     //Require at lest one Category
+    $(".roomCat").rules("add", {
+        required: true,
+        min: 0,
+        atLeastNInClass: {
+            class: ".roomCat",
+            min: 1
+        }
+    });
+
 });
