4b02149ef90ffada0aef83d0f4f34d4c4461506c
jhe6245 <jhe6245@students.fhv.at>
2021-11-16 17:44:29 +0100

    X Æ A-XII

--- src/main/resources/public/js/forms.js
+++ src/main/resources/public/js/forms.js
@@ -10,8 +10,7 @@ $(document).ready(function () {
         } else {
 
             $("input[type='text']").off("change");
-            $(".guestData").val("");
-            $(".guestData").prop("readonly", false);
+            $(".guestData").val("").prop("readonly", false);
 
         }
     });
