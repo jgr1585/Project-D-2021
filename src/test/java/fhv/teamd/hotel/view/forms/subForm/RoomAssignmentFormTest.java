a2ea5489cdbab9394125da589d4600f4e50f00fd
jhe6245 <jhe6245@students.fhv.at>
2021-11-25 11:22:56 +0100

    Changes by julian

---
+++ src/test/java/fhv/teamd/hotel/view/forms/subForm/RoomAssignmentFormTest.java
@@ -0,0 +1,15 @@
+package fhv.teamd.hotel.view.forms.subForm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RoomAssignmentFormTest {
+
+    @Test
+    void test() {
+        Assertions.fail();
+
+    }
+}
