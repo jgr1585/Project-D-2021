22e2953c9dcbb90c240e416c4f43d593fbd2586e
jgr1585 <jgr1585@students.fhv.at>
2021-11-28 21:13:46 +0100

    Created Test for Category

--- src/test/java/fhv/teamd/hotel/domain/CategoryTests.java
+++ src/test/java/fhv/teamd/hotel/domain/CategoryTests.java
@@ -1,13 +1,30 @@
 package fhv.teamd.hotel.domain;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class CategoryTests {
-    @Test
-    void test() {
-        Assertions.fail();
+
+    private Category cat1;
+    private Category cat2;
+
+
+    @BeforeEach
+    public void init() {
+        this.cat1 = new Category("Category 1", "Category 1", 20);
+        this.cat2 = new Category("Category 2", "Category 2", 30);
     }
+
+    @SuppressWarnings({ "SimplifiableAssertion", "EqualsWithItself", "ConstantConditions", "EqualsBetweenInconvertibleTypes" })
+    @Test
+    public void testEquals() {
+        Assertions.assertTrue(this.cat1.equals(this.cat1));
+        Assertions.assertFalse(this.cat1.equals(this.cat2));
+        Assertions.assertFalse(this.cat1.equals(null));
+        Assertions.assertFalse(this.cat1.equals(""));
+    }
+
 }
