5bf5d897ec54642e155b6af413b6665e4c55fcc7
jgr1585 <jgr1585@students.fhv.at>
2021-11-28 21:25:15 +0100

    Created Test for Room

--- src/test/java/fhv/teamd/hotel/domain/RoomTests.java
+++ src/test/java/fhv/teamd/hotel/domain/RoomTests.java
@@ -1,13 +1,30 @@
 package fhv.teamd.hotel.domain;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class RoomTests {
-    @Test
-    void test() {
-        Assertions.fail();
+
+    private Room room1;
+    private Room room2;
+
+    @BeforeEach
+    public void init() {
+        this.room1 = ReflectionUtils.newInstance(Room.class);
+        this.room2 = ReflectionUtils.newInstance(Room.class);
     }
+
+    @SuppressWarnings({ "SimplifiableAssertion", "EqualsWithItself", "ConstantConditions", "EqualsBetweenInconvertibleTypes" })
+    @Test
+    public void testEquals() {
+        Assertions.assertTrue(this.room1.equals(this.room1));
+        Assertions.assertFalse(this.room1.equals(this.room2));
+        Assertions.assertFalse(this.room1.equals(null));
+        Assertions.assertFalse(this.room1.equals(""));
+    }
+
 }
