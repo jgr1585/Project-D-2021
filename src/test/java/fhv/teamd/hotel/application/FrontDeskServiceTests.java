b8e72fe76e8a8fcc7d4cbbb394534d9253f22353
jgr1585 <jgr1585@students.fhv.at>
2021-11-28 18:44:22 +0100

    Add some more Tests to FrontDeskServiceTests Class

--- src/test/java/fhv/teamd/hotel/application/FrontDeskServiceTests.java
+++ src/test/java/fhv/teamd/hotel/application/FrontDeskServiceTests.java
@@ -1,6 +1,14 @@
 package fhv.teamd.hotel.application;
 
 import fhv.teamd.hotel.application.dto.StayDTO;
+import fhv.teamd.hotel.domain.Room;
+import fhv.teamd.hotel.domain.Stay;
+import fhv.teamd.hotel.domain.StayingState;
+import fhv.teamd.hotel.domain.contactInfo.Address;
+import fhv.teamd.hotel.domain.contactInfo.GuestDetails;
+import fhv.teamd.hotel.domain.contactInfo.PaymentMethod;
+import fhv.teamd.hotel.domain.contactInfo.RepresentativeDetails;
+import fhv.teamd.hotel.domain.ids.StayId;
 import fhv.teamd.hotel.domain.repositories.StayRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,7 +17,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 public class FrontDeskServiceTests {
@@ -24,6 +37,33 @@ public class FrontDeskServiceTests {
     void given_emptyRepository_when_getAll_returnsEmpty() {
         Mockito.when(this.stayRepository.getActiveStays()).thenReturn(Collections.emptyList());
         Assertions.assertEquals(0, this.frontDeskService.getActiveStays().size());
+    }
+
+    void given_emptyRepository_when_getAll_returnsAll() {
+        final LocalDateTime yesterday = LocalDateTime.now().minus(Period.ofDays(1));
+        final LocalDateTime now = LocalDateTime.now();
+        final LocalDateTime tomorrow = LocalDateTime.now().plus(Period.ofDays(1));
+
+        final Address addr = new Address("musterstrasse 1", "1234", "musterort", "musterland");
+
+        final GuestDetails guest = new GuestDetails("max", "muster", addr);
+
+        final RepresentativeDetails rep = new RepresentativeDetails(
+                "max","muster","m@mail.com", addr,"123456",
+                "1111 1111 1111 1111", PaymentMethod.CreditCard);
+
+        List<Stay> allStays = List.of(
+            new Stay(new StayId("stay-1"), now, tomorrow, new HashSet<>(), guest, rep, StayingState.CheckedIn),
+            new Stay(new StayId("stay-1"), yesterday, now, new HashSet<>(), guest, rep, StayingState.CheckedOut)
+        );
+
+        Mockito.when(this.stayRepository.getActiveStays()).thenReturn(allStays);
+
+        final List<StayDTO> actual = this.frontDeskService.getActiveStays();
+
+        final List<StayDTO> expected = allStays.stream().map(StayDTO::fromStay).collect(Collectors.toList());
+
+        Assertions.assertTrue(actual.containsAll(expected) && expected.containsAll(actual));
     }
 
 }
