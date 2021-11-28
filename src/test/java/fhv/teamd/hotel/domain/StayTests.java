8ad472c5fb8ad604d63e3eada961f67382ee3c41
jgr1585 <jgr1585@students.fhv.at>
2021-11-28 21:46:40 +0100

    Created Test for Stay

--- src/test/java/fhv/teamd/hotel/domain/StayTests.java
+++ src/test/java/fhv/teamd/hotel/domain/StayTests.java
@@ -1,13 +1,76 @@
 package fhv.teamd.hotel.domain;
 
+import fhv.teamd.hotel.domain.contactInfo.Address;
+import fhv.teamd.hotel.domain.contactInfo.GuestDetails;
+import fhv.teamd.hotel.domain.contactInfo.PaymentMethod;
+import fhv.teamd.hotel.domain.contactInfo.RepresentativeDetails;
+import fhv.teamd.hotel.domain.exceptions.AlreadyCheckedOutException;
+import fhv.teamd.hotel.domain.ids.StayId;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootTest
 public class StayTests {
-    @Test
-    void test() {
-        Assertions.fail();
+
+    private Stay stay1;
+    private Stay stay2;
+
+    @BeforeEach
+    public void init() {
+        final LocalDateTime yesterday = LocalDateTime.now().minus(Period.ofDays(1));
+        final LocalDateTime now = LocalDateTime.now();
+        final LocalDateTime tomorrow = LocalDateTime.now().plus(Period.ofDays(1));
+
+        @SuppressWarnings("SpellCheckingInspection")
+        final Address addr = new Address("musterstrasse 1", "1234", "musterort", "musterland");
+
+        final GuestDetails guest = new GuestDetails("max", "muster", addr);
+
+        final RepresentativeDetails rep = new RepresentativeDetails(
+                "max","muster","m@mail.com", addr,"123456",
+                "1111 1111 1111 1111", PaymentMethod.CreditCard);
+
+        final Room room1 = ReflectionUtils.newInstance(Room.class);
+        final Set<Room> rooms1 = new HashSet<>();
+        rooms1.add(room1);
+
+        final Room room2 = ReflectionUtils.newInstance(Room.class);
+        final Set<Room> rooms2 = new HashSet<>();
+        rooms2.add(room2);
+
+        this.stay1 = new Stay(new StayId("stay-1"), now, tomorrow, rooms1, guest, rep, StayingState.CheckedIn);
+        this.stay2 = new Stay(new StayId("stay-2"), yesterday, now, rooms2, guest, rep, StayingState.CheckedOut);
     }
+
+    @Test
+    public void testCheckOut() {
+        Assertions.assertTrue(this.stay1.isActive());
+        Assertions.assertFalse(this.stay2.isActive());
+
+        try {
+            this.stay1.checkOut();
+        } catch (AlreadyCheckedOutException ignored) {}
+
+        Assertions.assertThrows(AlreadyCheckedOutException.class, this.stay2::checkOut);
+        Assertions.assertFalse(this.stay1.isActive());
+        Assertions.assertFalse(this.stay2.isActive());
+    }
+
+    @SuppressWarnings({ "SimplifiableAssertion", "EqualsWithItself", "EqualsBetweenInconvertibleTypes", "ConstantConditions" })
+    @Test
+    public void testEquals() {
+        Assertions.assertTrue(this.stay1.equals(this.stay1));
+        Assertions.assertFalse(this.stay1.equals(this.stay2));
+        Assertions.assertFalse(this.stay1.equals(""));
+        Assertions.assertFalse(this.stay1.equals(null));
+    }
+
 }
