e4c7bb784b202fd1d99c4f438aed2821d77fd28e
jgr1585 <jgr1585@students.fhv.at>
2021-12-13 23:17:34 +0100

    Created some more Test

--- src/test/java/fhv/teamd/hotel/infrastructure/CategoryRepositoryTests.java
+++ src/test/java/fhv/teamd/hotel/infrastructure/CategoryRepositoryTests.java
@@ -1,14 +1,38 @@
 package fhv.teamd.hotel.infrastructure;
 
+import fhv.teamd.hotel.domain.Category;
+import fhv.teamd.hotel.domain.Room;
+import fhv.teamd.hotel.domain.repositories.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class CategoryRepositoryTests {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Test
-    void test() {
-        Assertions.fail();
+    void given_category_when_getAll_return_all() {
+        List<Category> expected = BaseRepositoryData.categories();
+        List<Category> actual = this.categoryRepository.getAll();
+
+        Assertions.assertTrue(expected.containsAll(actual) && actual.containsAll(expected));
+    }
+
+    @Test
+    void given_category_when_findById_return_categoryById() {
+        List<Category> categories = BaseRepositoryData.categories();
+
+        categories.forEach(category -> {
+            @SuppressWarnings("OptionalGetWithoutIsPresent")
+            Category actual = this.categoryRepository.findById(category.categoryId()).get();
+
+            Assertions.assertEquals(category, actual);
+        });
     }
 }
