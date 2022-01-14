e092526da0c47f993bbf83641ec39f8480d0ce39
alexa <alexander.gort@students.fhv.at>
2022-01-14 17:14:41 +0100

    improvment aber no engine

--- spa_src/src/components/booking/ChooseCategories.js
+++ spa_src/src/components/booking/ChooseCategories.js
@@ -1,22 +1,25 @@
 import React, {PureComponent} from 'react'
 
-
 import withStyles from '@mui/styles/withStyles';
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
 
-import {Container, TextField} from "@mui/material";
+import {Box, Container, FormControl, InputLabel, MenuItem, Select, TextField} from "@mui/material";
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import StaticDatePicker from '@mui/lab/StaticDatePicker';
 import {DatePicker} from "@mui/lab";
 
 const styles = () => ({});
 
 class ChooseCategories extends PureComponent {
-    onDateSelectChange(value, chooseCategory) {
+    onDateSelectChange = (value, chooseCategory) => {
 
-    }
+    };
+
+
+    onCategorySelect = (value, chooseCategory) => {
+
+    };
+
 
     render() {
         const {classes, chooseCategory} = this.props;
@@ -46,6 +49,50 @@ class ChooseCategories extends PureComponent {
                             renderInput={(params) => <TextField {...params} />}
                         />
                     </LocalizationProvider>
+                </Container>
+
+                <Container maxWidth="sm">
+                    <Box sx={{ minWidth: 10 }}>
+                        <FormControl sx={{ m: 1, minWidth: 120 }}>
+                            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={"single-Room"}
+                                label="Single"
+                                onChange={(newValue) => {
+                                    this.onCategorySelect(newValue, chooseCategory);
+                                }}
+                            >
+                                <MenuItem value={1}>1</MenuItem>
+                                <MenuItem value={2}>2</MenuItem>
+                                <MenuItem value={3}>3</MenuItem>
+                                <MenuItem value={5}>4</MenuItem>
+                                <MenuItem value={5}>5</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+
+                    <Box sx={{ minWidth: 10 }}>
+                        <FormControl sx={{ m: 1, minWidth: 120 }}>
+                            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={"double-Room"}
+                                label="Double"
+                                onChange={(newValue) => {
+                                    this.onCategorySelect(newValue, chooseCategory);
+                                }}
+                            >
+                                <MenuItem value={1}>1</MenuItem>
+                                <MenuItem value={2}>2</MenuItem>
+                                <MenuItem value={3}>3</MenuItem>
+                                <MenuItem value={5}>4</MenuItem>
+                                <MenuItem value={5}>5</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
                 </Container>
             </React.Fragment>
         )
