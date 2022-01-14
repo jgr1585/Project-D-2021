ce870b9cf7fcc176a46d0b1796e97278ef62c152
alexa <alexander.gort@students.fhv.at>
2022-01-14 15:17:29 +0100

    react choose cat

--- spa_src/src/components/booking/ChooseCategories.js
+++ spa_src/src/components/booking/ChooseCategories.js
@@ -1,19 +1,17 @@
-
-import React, { PureComponent } from 'react'
+import React, {PureComponent} from 'react'
 
 
 import withStyles from '@mui/styles/withStyles';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
-import {TextField} from "@mui/material";
+import {Container, TextField} from "@mui/material";
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import StaticDatePicker from '@mui/lab/StaticDatePicker';
+import {DatePicker} from "@mui/lab";
 
-const styles = () => ({
-
-});
+const styles = () => ({});
 
 class ChooseCategories extends PureComponent {
     onDateSelectChange(value, chooseCategory) {
@@ -21,23 +19,34 @@ class ChooseCategories extends PureComponent {
     }
 
     render() {
-        const { classes, chooseCategory } = this.props;
+        const {classes, chooseCategory} = this.props;
 
         return (
             <React.Fragment>
+                <Container maxWidth="sm">
 
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                    <StaticDatePicker
-                        orientation="landscape"
-                        openTo="day"
-                        value={new Date()}
-                        onChange={(newValue) => {
-                            this.onDateSelectChange(newValue, chooseCategory);
-                        }}
-                        renderInput={(params) => <TextField {...params} />}
-                    />
-                </LocalizationProvider>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DatePicker
+                            label="From Date"
+                            value={new Date()}
+                            onChange={(newValue) => {
+                                this.onDateSelectChange(newValue, chooseCategory);
+                            }}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </LocalizationProvider>
 
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DatePicker
+                            label="Todo Date"
+                            value={new Date()}
+                            onChange={(newValue) => {
+                                this.onDateSelectChange(newValue, chooseCategory);
+                            }}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </LocalizationProvider>
+                </Container>
             </React.Fragment>
         )
     }
