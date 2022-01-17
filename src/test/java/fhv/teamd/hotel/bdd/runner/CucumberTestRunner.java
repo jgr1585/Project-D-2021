ff7cd80808ad88d83f769a9a894125ee89a3760d
jhe6245 <jhe6245@students.fhv.at>
2022-01-17 18:13:54 +0100

    doch ned

--- src/test/java/fhv/teamd/hotel/bdd/runner/CucumberTestRunner.java
+++ src/test/java/fhv/teamd/hotel/bdd/runner/CucumberTestRunner.java
@@ -2,7 +2,6 @@ package fhv.teamd.hotel.bdd.runner;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
@@ -10,8 +9,4 @@ import org.junit.runner.RunWith;
         glue = { "fhv.teamd.hotel.bdd" },
         plugin = { "pretty", "html:build/cucumber/hotel.html" })
 public class CucumberTestRunner {
-    @Test
-    public void dummyTest() {
-        // gradle won't execute otherwise
-    }
 }
