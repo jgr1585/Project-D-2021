8449fdc8df3c19ea8669fa9e150793fe26b599a2
jgr1585 <jgr1585@students.fhv.at>
2022-01-18 11:18:46 +0100

    Add basic Function for Booking

--- src/main/java/fhv/teamd/hotel/rest/BookingController.java
+++ src/main/java/fhv/teamd/hotel/rest/BookingController.java
@@ -1,9 +1,13 @@
 package fhv.teamd.hotel.rest;
 
 import fhv.teamd.hotel.application.BookingService;
+import fhv.teamd.hotel.domain.contactInfo.GuestDetails;
+import fhv.teamd.hotel.domain.contactInfo.RepresentativeDetails;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.Map;
 
 
 @RestController
@@ -12,5 +16,17 @@ public class BookingController {
 
     @Autowired
     private BookingService bookingService;
+
+    @PostMapping("/book")
+    @ResponseBody
+    public void book(@RequestParam Map<String, Integer> categoryIdsAndAmounts,
+                     @RequestParam LocalDate from, @RequestParam LocalDate until,
+                     @RequestParam GuestDetails guest, @RequestParam RepresentativeDetails representative) throws Exception {
+
+        //TODO: Finish
+        this.bookingService.book(categoryIdsAndAmounts, from.atStartOfDay(), until.atStartOfDay(), guest, representative, null);
+
+    }
+
 
 }
